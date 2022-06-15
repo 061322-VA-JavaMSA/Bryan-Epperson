52f0569f530509a9173a0b9799bcdb9932019ac0
eppersonb <eppersonb@berea.edu>
2022-06-15 18:14:03 -0400

    Scanner assignment, kinda overthought it

--- projects/AccountAuthentication/src/main/java/Adventure.java
+++ projects/AccountAuthentication/src/main/java/Adventure.java
@@ -1,4 +1,33 @@
+import java.util.Scanner;
 
 public class Adventure {
+	
+	static Scanner scan = new Scanner(System.in);
+	
+	public static void startMenu() {
+		System.out.println("Choose the start of your adventure!");
+		System.out.println("1: Go take a hike in the forest!");
+		System.out.println("2: Go to the local tavern!");
+		System.out.println("3: Go to the Mountains!");
+		System.out.println("4: Go visit your grandmother!");
+		String userInput = scan.nextLine();
+		switch(userInput) {
+		case "1":
+			theForest();
+		default:
+			System.out.println("Thats not a valid option!");
+			System.out.println("----------------------------------");
+			startMenu();
+			break;
+		}
+
+		
+	}
+	public static void theForest() {
+		System.out.println("Welcome to the forest!");
+	}
+	public static void main(String[] args) {
+		startMenu();
+	};
 
 }
