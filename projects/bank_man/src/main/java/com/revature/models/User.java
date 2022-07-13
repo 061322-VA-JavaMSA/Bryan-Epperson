3db1a6b8a9f8bd463427794917663e2a5a72fce7
eppersonb <eppersonb@berea.edu>
2022-07-13 15:47:29 -0400

    Server now has a clear connection with the database

--- projects/bank_man/src/main/java/com/revature/models/User.java
+++ projects/bank_man/src/main/java/com/revature/models/User.java
@@ -12,7 +12,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="users")
+@Table(name="users", schema = "public")
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
